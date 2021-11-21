ad6ec58e16fb7285042158450915496d092c3019
Nic <tmninh94@gmail.com>
2021-11-21 23:16:59 +0700

    "Nic"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,9 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-12">
-                <a href="{{ asset('document.html') }}" target="_blank">{{ trans('Document') }}</a>
-            </div>
+{{--            <div class="col-12">--}}
+{{--                <a href="{{ asset('document.html') }}" target="_blank">{{ trans('Document') }}</a>--}}
+{{--            </div>--}}
             <div class="col-12 mt-5">
                 <div class="bg-light p-5 rounded row">
                     <div class="col-8">
